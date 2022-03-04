dd77e7d5b7ce95caabc837457ee211d734606ab0
fateme <ftme.ahsan@gmail.com>
2022-03-05 00:01:34 +0330

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,93 @@
 #In the name of God
-from numpy import random
-for i in range(1, 101):
-    file1 = open("in/input" + str(i) + ".txt", "w")
-    n = 20 - random.randint(20 - 1)
-    k = 10 - random.randint(9)
-    file1.write(str(n) + " " + str(k))
-    for j in range(k):
-        x = n - random.randint(n - 1)
-        y = n - random.randint(n - 1)
-        file1.write("\n" + str(x) + " " + str(y))
+
+from socket import *
+import ssl
+import base64
+
+mailServer = "smtp.gmail.com"
+
+port = 587
+
+to = "*@gmail.com"
+
+From = "*@gmail.com"
+
+emailMessage = "Salam Fateme"
+
+domainName = "Fateme"
+
+SMTPSocket = socket(AF_INET, SOCK_STREAM)
+SMTPSocket.connect((mailServer, port))
+
+rcv = SMTPSocket.recv(1024)
+print(rcv)
+
+command = "HELO "+ domainName +"\r\n"
+SMTPSocket.send(command.encode())
+
+rcv = SMTPSocket.recv(1024)
+print(rcv)
+
+command = 'STARTTLS\r\n'
+SMTPSocket.send(command.encode())
+
+rcv = SMTPSocket.recv(1024)
+print(rcv)
+
+sslSMTPSocket = ssl.wrap_socket(SMTPSocket)
+
+command = "AUTH LOGIN\r\n"
+sslSMTPSocket.send(command.encode())
+
+rcv = sslSMTPSocket.recv(1024)
+print(rcv)
+
+encoded = base64.b64encode(From.encode())
+command = encoded.decode('ascii') + "\r\n"
+sslSMTPSocket.send(command.encode())
+
+rcv = sslSMTPSocket.recv(1024)
+print(rcv)
+
+# *********** password removed for security
+encoded = base64.b64encode("***********".encode())
+command = encoded.decode('ascii') + "\r\n"
+sslSMTPSocket.send(command.encode())
+
+rcv = sslSMTPSocket.recv(1024)
+print(rcv)
+
+command = "MAIL FROM: <" + From + ">\r\n"
+sslSMTPSocket.send(command.encode())
+
+rcv = sslSMTPSocket.recv(1024)
+print(rcv)
+
+command = "RCPT TO: <" + to + ">\r\n"
+sslSMTPSocket.send(command.encode())
+
+rcv = sslSMTPSocket.recv(1024)
+print(rcv)
+
+command = 'DATA\r\n'
+sslSMTPSocket.send(command.encode())
+
+rcv = sslSMTPSocket.recv(1024)
+print(rcv)
+
+command = emailMessage + "\r\n"
+sslSMTPSocket.send(command.encode())
+
+command = ".\r\n"
+sslSMTPSocket.send(command.encode())
+
+rcv = sslSMTPSocket.recv(1024)
+print(rcv)
+
+command = 'QUIT\r\n'
+sslSMTPSocket.send(command.encode())
+
+rcv = sslSMTPSocket.recv(1024)
+print(rcv)
+
+sslSMTPSocket.close()
